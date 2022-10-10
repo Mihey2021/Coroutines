dc69df91238b1aae77bd7835e7bccb5c82c92413
Mihey2021 <hunter_m@list.ru>
2022-10-10 21:56:55 +0300

    Initial commit

---
+++ src/main/kotlin/dto/Post.kt
@@ -0,0 +1,13 @@
+package dto
+
+import java.io.Serializable
+
+data class Post(
+    val id: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+    var attachment: Unit? = null,
+)
